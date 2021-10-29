772ccbcd944d109bb48a4a8ffa0562f66accb871
gregfalla <he201939@students.ephec.be>
2021-10-30 00:57:36 +0200

    Update login page

--- app/src/main/java/com/example/myapplication/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/login/LoginViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class LoginViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "EPHEC-Foods"
     }
     val text: LiveData<String> = _text
 }
