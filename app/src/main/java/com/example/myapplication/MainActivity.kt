3dc2dcf976c9cc9c4d9caa5952d3ef4a950a8704
gregfalla <he201939@students.ephec.be>
2021-10-30 16:20:08 +0200

    Update page "choix"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_login, R.id.nav_commandes, R.id.nav_param, R.id.nav_repas
+                R.id.nav_login, R.id.nav_commandes, R.id.nav_repas
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
