49bc03b137a931d5667eaff0026cd8b2e1bb8083
gregfalla <he201939@students.ephec.be>
2021-10-30 16:20:08 +0200

    Update page "choix"

--- app/src/main/java/com/example/myapplication/ui/parametres/ParametreViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/parametres/ParametreViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ParametreViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Ttestt"
     }
     val text: LiveData<String> = _text
 }
